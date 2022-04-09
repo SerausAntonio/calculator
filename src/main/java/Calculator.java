af4c6874929f53d3c2367b17a47af6d881dd6028
Antonio Seraus <aseraus@hotmail.com>
2022-04-10 00:33:17 +0200

    print Multiply

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,11 +4,13 @@ public class Calculator {
         System.out.println(Add(45,67));
 
         System.out.println(Sub(45,78));
+
+        System.out.println(Multiply(23, 5));
     }
     //https://www.youtube.com/watch?v=hufGg2mf7eA
 
 
-      public static double Add(int x, int y) {
+    public static double Add(int x, int y) {
         return (x * y);
     }
 
@@ -16,5 +18,9 @@ public class Calculator {
         return (x-y);
     }
 
+    public static double Multiply(int x, int y){
+
+        return (x*y);
+    }
 
 }

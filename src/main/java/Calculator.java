b5ebd0acc9ecf78d866a96187bd6a66a0c088569
Antonio Seraus <aseraus@hotmail.com>
2022-04-10 00:14:14 +0200

    Initial Commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static void main(String args[]) {
+
+        System.out.println(Add(45,67));
+    }
+    //https://www.youtube.com/watch?v=hufGg2mf7eA
+
+
+      public static double Add(int x, int y) {
+        return (x * y);
+    }
+
+    public static double Sub(int x, int y){
+        return (x-y);
+    }
+
+
+}
